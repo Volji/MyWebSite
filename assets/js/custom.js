3f96c79be043470e9aad268d5956c2dc0b1c22d4
volji <chipset@laposte.net>
2018-07-24 18:10:07 +0200

    First Commit

---
+++ assets/js/custom.js
@@ -0,0 +1,8 @@
+// Typed Plugin
+typed = new Typed('.type', {
+		strings: ["Chef de projet", "Développeur Fullstack", "Manager IT"], /* Here Type Your Title */
+		typeSpeed: 40,
+		loop: true,
+		backDelay: 1200,
+		backSpeed: 20
+});
